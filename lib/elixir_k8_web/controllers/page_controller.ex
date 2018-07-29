22aee13791c00bb8b2ebc7d807f21776c398852d
Aravind C Ranjan <aravindcranjan54@gmail.com>
2018-07-29 23:12:35 +0530

    changed ip

--- lib/elixir_k8_web/controllers/page_controller.ex
+++ lib/elixir_k8_web/controllers/page_controller.ex
@@ -7,10 +7,8 @@ defmodule ElixirK8Web.PageController do
   end
 
   def hello(conn,_params) do
-    IO.puts(Application.get_env(:elixir_k8,ElixirK8Web.Endpoint)[:dogstatsd])
-    IO.puts(:ip)
-    IO.puts(System.get_env("DD_KUBERNETES_KUBELET_HOST"))
-    {:ok,statsd} = DogStatsd.new(Application.get_env(:elixir_k8,ElixirK8Web.Endpoint)[:dogstatsd], 8125)
+    
+    {:ok,statsd} = DogStatsd.new("10.128.0.13", 8125)
     IO.puts("here")
     DogStatsd.increment(statsd, "fucking.asshole")
     IO.puts("Here")
