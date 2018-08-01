74a3170dea5fce8fb41cc194c081855bed93cdfb
Aravind C Ranjan <aravindcranjan54@gmail.com>
2018-08-01 12:00:23 +0530

    bug fix

--- lib/elixir_k8_web/controllers/page_controller.ex
+++ lib/elixir_k8_web/controllers/page_controller.ex
@@ -10,7 +10,7 @@ defmodule ElixirK8Web.PageController do
     IO.puts(System.get_env("DD_KUBERNETES_KUBELET_HOST"))
     {:ok,statsd} = DogStatsd.new(System.get_env("DD_KUBERNETES_KUBELET_HOST"), 8125)
     IO.puts("here")
-    DogStatsd.increment(statsd, "fucking.asshole")
+    DogStatsd.increment(statsd, "testingk8")
     IO.puts("Here")
     text conn, "hello"
   end
