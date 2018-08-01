e3b9f91dd523238b844c4273095748dacdc03775
Aravind C Ranjan <aravindcranjan54@gmail.com>
2018-08-01 10:22:14 +0530

    fixed datadog daemonset-fix dogstatsd

--- lib/elixir_k8_web/controllers/page_controller.ex
+++ lib/elixir_k8_web/controllers/page_controller.ex
@@ -9,9 +9,7 @@ defmodule ElixirK8Web.PageController do
   def hello(conn,_params) do
     IO.puts(System.get_env("DD_KUBERNETES_KUBELET_HOST"))
     {:ok,statsd} = DogStatsd.new(System.get_env("DD_KUBERNETES_KUBELET_HOST"), 8125)
-    IO.puts("here")
-    DogStatsd.increment(statsd, "fucking.asshole")
-    IO.puts("Here")
+    DogStatsd.increment(statsd, "testingK8")
     text conn, "hello"
   end
 end
